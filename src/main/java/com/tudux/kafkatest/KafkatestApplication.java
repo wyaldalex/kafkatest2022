fec5244aa31351c004ac9657107ee19dcfa6cf3d
AlexAR <moltron_nano@hotmail.com>
2022-08-19 17:14:58 -0600

    feat: initial commit for kafka test project

---
+++ src/main/java/com/tudux/kafkatest/KafkatestApplication.java
@@ -0,0 +1,13 @@
+package com.tudux.kafkatest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkatestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkatestApplication.class, args);
+	}
+
+}
